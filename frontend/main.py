be40789803cf359468d7c8d34411e052de8f0558
Soniaswani <sswani@deloitte.com>
2019-08-21 15:30:21 -0500

    Update to create html

--- frontend/main.py
+++ frontend/main.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, request, redirect, abort, jsonify
-#hi
+
 app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
 @app.route("/Events")
 def events():
-    return render_template("Events.html")    
+    return render_template("Events.html")
+
+
+@app.route("/create")
+def createvent():
+    return render_template("create.html")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
