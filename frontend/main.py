e13d833438612f3808225b9859403d11be3e135e
Soniaswani <sswani@deloitte.com>
2019-08-21 10:40:16 -0500

    Test

--- frontend/main.py
+++ frontend/main.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-    
+@app.route("/Events")
+def events():
+    return render_template("Events.html")    
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
