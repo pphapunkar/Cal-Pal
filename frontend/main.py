f213eb71b5f98034b761e39ca82ea242cec9b7e6
madcaldwell <madcaldwell@deloitte.com>
2019-08-22 09:33:48 -0500

    Trying something different

--- frontend/main.py
+++ frontend/main.py
@@ -1,27 +1,26 @@
 from flask import Flask, render_template, request, redirect, abort, jsonify
-
+import requests
 URL = 'https://backend-dot-cal-pal-250416.appspot.com/Events'
 
 app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
-
-
 @app.route("/Events")
 def events():
-    data = request.get(URL + '/Events')
+    data = request.get(URL)
     print(data)
     return render_template("Events.html")
-
-
-@app.route("/create", methods=['POST'])
-def createvent():
-    url = URL + '/create'
-    data = request.form.to_dict(flat=True)
-    print(request.post(url, data=data))
+@app.route("/addEvents")  
+def addEvent():
     return render_template("create.html")
 
-
+@app.route("/Events/create", methods = ['POST'])
+def createvent():
+    print('URL--' + URL)
+    url = URL + '/events/add/' + str(uuid.uuid4())
+    data = request.form.to_dict(flat=True)
+    print(requests.post(url, data=data))
+    return redirect('/')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
